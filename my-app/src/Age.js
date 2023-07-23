dc3c045238bc1897f8dcb3e46b34c6902de31430
Sandro Grimaldi <sandrogrimaldi@outlook.it>
2023-07-23 16:02:51 +0200

    you are very young

--- my-app/src/Age.js
+++ my-app/src/Age.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from "react"
 
 export class Age extends React.Component {
     render() {
-        return <p>Your age is {this.props.age}</p> 
+        return <p>{this.props.age > 18 ? `Your age is ${this.props.age}` : 'You are too young!'}</p>
     }
 }
