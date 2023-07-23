431f934bfc6c2c2fd704853f4232c27799fa2754
Sandro Grimaldi <sandrogrimaldi@outlook.it>
2023-07-23 16:18:13 +0200

    componentDidMount

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 export class Counter extends React.Component{
     state={count: this.props.initialValue ?? 0}
-    constructor(props){
+    /*constructor(props){
         super(props)
         setInterval(()=>{this.setState((state)=>{return{count: state.count + (this.props.increment ?? 1)}})},(this.props.incrementInterval ?? 1000))
-    }
+    }*/
+    componentDidMount() {
+        this.setupConnection();
+      }
     render(){
         return<div>
             <h1>Count: {this.state.count}</h1>
         </div>
     }
 }
+/* il costruttore non possiamo toglierlo altrimenti non si può aggiornare lo stato */
