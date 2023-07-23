a6d7f3e62cba3e306e0fafa80a5ddf31c2753b1d
Sandro Grimaldi <sandrogrimaldi@outlook.it>
2023-07-23 15:41:57 +0200

    grater than 18 done

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -4,8 +4,8 @@ import { Age } from "./Age";
 export class Welcome extends React.Component {
     render() {
         return  <div>
-                    <p>Welcome, {this.props.name}</p>
-                    <Age age={this.props.age} />
+                    <p>Welcome, {this.props.name}</p>    
+                    { this.props.age > 18 && <Age age={this.props.age} />}
                 </div>
     }
 }
